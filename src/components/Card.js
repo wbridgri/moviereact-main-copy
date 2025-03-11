cba704194bd7a928cf398c9d20ca3aa1273c60ad
wbridgri <will.bridges@comcast.net>
2025-03-11 14:00:39 -0500

    please work

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,7 +8,7 @@ const Card = (props) => {
     return (
         <div className="col">
             <Link to={`../Movies/${props.id}`} key={props.id}>
-                <div className="card travel-card">
+                <div className="card">
                     <img src={props.imgUrl}alt="placeholder img"className="img-fluid image card-img-top rounded" />
                     <div className="card-header">
                         <p className="card-text text-center">
